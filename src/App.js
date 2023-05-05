923092b8b8aa221c65b21ba4166fe12a2b079408
LuisMi01 <lmurbezv@gmail.com>
2023-05-05 19:33:17 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,69 +1,86 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { create, all } from 'mathjs';
+
 function App() {
-    const [calc, setCalc] = useState("");
-    const [result, setResult] = useState("");;
+    const [calc, setCalc] = useState('');
+    const [result, setResult] = useState('');
 
-    const ops = ["/", "*", "+", "-", "."];
+    const ops = ['/', '*', '+', '-', '.'];
+    const math = create(all);
 
-    const updateCalc = value => {
-        if(
-            ops.includes(value) && calc === "" ||
-            ops.includes(value) && ops.includes(calc.slice(-1))
-        ){
+    const updateCalc = (value) => {
+        if (
+            ops.includes(value) &&
+            (calc === '' || ops.includes(calc.slice(-1)))
+        ) {
             return;
         }
 
-        if(!ops.includes(value)){
-            setResult(eval(calc + value ).toString())
+        if (!ops.includes(value)) {
+            try {
+                setResult(math.evaluate(calc + value).toString());
+            } catch (error) {
+                // si hay un error, simplemente no hacemos nada
+            }
         }
-    }
+        setCalc(calc + value);
+    };
 
     const calculate = () => {
-        setCalc(eval(calc).toString())
-    }
+        try {
+            setCalc(math.evaluate(calc).toString());
+        } catch (error) {
+            // si hay un error, simplemente no hacemos nada
+        }
+    };
 
     const deleteLast = () => {
-        if(calc == ""){
+        if (calc === '') {
             return;
         }
-        const value = calc.slice(0, -1)
-        setCalc(value)
-    }
-    const crearDigitos = ()=> {
+        const value = calc.slice(0, -1);
+        setCalc(value);
+    };
+
+    const crearDigitos = () => {
         const digitos = [];
         for (let i = 1; i < 10; i++) {
             digitos.push(
-                <button onClick={()=> updateCalc(i.toString())} key={i}>{i}</button>
-            )
+                <button onClick={() => updateCalc(i.toString())} key={i}>
+                    {i}
+                </button>
+            );
         }
         return digitos;
-    }
-  return (
-    <div className="App">
-      <div className="calculadora">
-        <div className="display">
-            {result ? <span>{result}</span> : ''} &nbsp; {calc || "0"} <span>{result}</span>
+    };
+
+    return (
+        <div className="App">
+            <div className="calculadora">
+                <div className="display">
+                    <span className="result">{result && `(${result})`}</span> &nbsp;
+                    <span className="operation">{calc || '0'}</span>
+                </div>
+
+
+                <div className="operadores">
+                    <button onClick={() => updateCalc('+')}>+</button>
+                    <button onClick={() => updateCalc('-')}>-</button>
+                    <button onClick={() => updateCalc('*')}>*</button>
+                    <button onClick={() => updateCalc('/')}>/</button>
+
+                    <button onClick={deleteLast}>CE</button>
+                </div>
+
+                <div className="numeros">
+                    {crearDigitos()}
+                    <button onClick={() => updateCalc('0')}>0</button>
+                    <button onClick={() => updateCalc('.')}>.</button>
+                    <button onClick={calculate}>=</button>
+                </div>
+            </div>
         </div>
-
-          <div className="operadores">
-              <button onClick={()=> updateCalc('+')}>+</button>
-              <button onClick={()=> updateCalc('-')}>-</button>
-              <button onClick={()=> updateCalc('*')}>*</button>
-              <button onClick={()=> updateCalc('/')}>/</button>
-
-              <button onClick={deleteLast}>CE</button>
-          </div>
-
-          <div className="numeros">
-              {crearDigitos()}
-              <button onClick={()=> updateCalc('0')}>0</button>
-              <button onClick={()=> updateCalc('.')}>.</button>
-              <button onClick={calculate}>=</button>
-
-          </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
